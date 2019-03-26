86b7c9c24c8f70c030f5c2522cc08561faf2df96
Seroga Humenchuk <niva1999niva@gmail.com>
2019-03-26 12:24:34 +0200

    fix es-lint errors

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 import rootModule from '../modules/reducers/rootModule';
 
-const store = createStore(
-  rootModule,
-  composeWithDevTools(applyMiddleware(thunk)),
-);
+const store = createStore(rootModule, applyMiddleware(thunk));
 
 export default store;
